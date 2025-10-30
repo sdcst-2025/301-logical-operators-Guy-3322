817b333eb48cbbcf11ede1b32fa79a1cccbedb39
hungrybeagle <cyang@deltalearns.ca>
2025-10-29 17:45:17 -0700

    Add example of nested if statements in Python
    This file demonstrates the use of nested if statements to check the positivity of two numbers.

---
+++ example0.py
@@ -0,0 +1,19 @@
+# An example of nested if statements
+
+x = 10
+y = 20
+
+if x > 0:
+  if y > 0:
+    print("Both x and y are positive numbers!")
+  else:
+    print("Only x is positive")
+else:
+  if y > 0:
+    print("only y is positive")
+  else:
+    print("neither of them are positive")
+
+# nested if statements allow you to run an if/conditional statement within another conditional statement.
+# but there are better ways to check and see if both x and y are positive.  Look at the logical operators in the next few example files!
+    
